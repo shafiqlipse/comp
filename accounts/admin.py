149ba474a24948658620b2bf0d963115042078b1
shafiq <ssekittoshafiq@gmail.com>
2024-07-05 17:26:48 +0300

    res

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import *
+from dashboard.models import *
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Sport)
+admin.site.register(Championship)
+admin.site.register(Season)
+admin.site.register(School)
