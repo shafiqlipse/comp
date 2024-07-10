c525d1821c3821870f43b5dddd24d5cda7eab2e8
shafiq <ssekittoshafiq@gmail.com>
2024-07-10 13:14:38 +0300

    net

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from accounts.models import *
 from dashboard.models import *
+from netball.models import *
+from football.models import *
+from handball.models import *
+from volleyball.models import *
+from basketball3.models import *
+
 
 # Register your models here.
 admin.site.register(User)
@@ -10,3 +16,8 @@ admin.site.register(Sport)
 admin.site.register(Championship)
 admin.site.register(Season)
 admin.site.register(School)
+admin.site.register(Football)
+admin.site.register(Netball)
+admin.site.register(Handball)
+admin.site.register(Volleyball)
+admin.site.register(Basketball3)
